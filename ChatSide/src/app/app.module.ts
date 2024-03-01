74c8e2cb19c3d76a80f9f451771a69d68605922d
piecairo <p.cairo.2224@vallauri.edu>
2024-03-01 11:40:54 +0100

    Chat branch created

--- ChatSide/src/app/app.module.ts
+++ ChatSide/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    ChatComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
