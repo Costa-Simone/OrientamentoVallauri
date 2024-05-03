3be6d5bf4ffc2fc9563647f8ba808470ba749627
Migori <a.migori.2275@vallauri.edu>
2024-05-03 13:14:45 +0200

    Removed unnecessary component and added number of students in the group

--- Mobile/src/app/service/labs.service.ts
+++ Mobile/src/app/service/labs.service.ts
@@ -7,6 +7,7 @@ import { DataStorageService } from './data-storage.service';
 export class LabsService {
   labs:any[] = []
   groupId:any
+  groupNumber:number = 0
 
   constructor(protected dataStorage:DataStorageService) { }
 
@@ -17,8 +18,9 @@ export class LabsService {
   getLabs(id:any){
     this.dataStorage.InviaRichiesta('get',`/laboratoriByGruppo/${id}`)?.subscribe({
       "next":(data) => {
+        this.groupNumber = data.recordsets[0][data.recordsets[0].length-1].num_studenti
+        data.recordsets[0].pop()
         this.labs = data.recordsets[0]
-        console.log(this.labs)
       },
       "error": (e) => {
         console.log(e)
