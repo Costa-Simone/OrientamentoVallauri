618eafbb152b4c15bcb594b48dd7e38358a54bf6
Migori <a.migori.2275@vallauri.edu>
2024-06-18 16:46:22 +0200

    Added interceptors and tocken

--- Mobile/src/app/app.module.ts
+++ Mobile/src/app/app.module.ts
@@ -11,17 +11,23 @@ import { TabsComponent } from './component/tabs/tabs.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomePage } from './component/home/home.page';
 import { CameraComponent } from './component/camera/camera.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { LogInComponent } from './component/log-in/log-in.component';
 import { LabDetailComponent } from './component/lab-detail/lab-detail.component';
 import { ChatComponent } from './component/chat/chat.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { ChatListComponent } from './component/chat-list/chat-list.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { CustomInterceptor } from './interceptors/custom.interceptor';
 
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot([]), AppRoutingModule, IonicModule.forRoot({}), HttpClientModule,ReactiveFormsModule, FormsModule,MatMenuModule,MatButtonModule],
   declarations: [AppComponent, TabsComponent,HomePage,CameraComponent,LogInComponent,LabDetailComponent, ChatListComponent,ChatComponent],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: CustomInterceptor, multi: true},
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Mobile/src/app/service/socket.service.ts
+++ Mobile/src/app/service/socket.service.ts
@@ -2,24 +2,25 @@ import { Injectable } from '@angular/core';
 import { io } from 'socket.io-client';
 import { ChatService } from './chat.service';
 import {Channel, LocalNotifications} from '@capacitor/local-notifications';
+import { Capacitor } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SocketService {
   socket: any;
+  permission: any
 
   constructor(protected chatService: ChatService) {}
 
   GoOnline() {
-    this.socket = io('http://79.25.227.23:80', {
+    this.socket = io('http://10.88.202.145:3001', {
       withCredentials: true,
       extraHeaders: {
         'my-custom-header': 'abcd',
       },
     });
 
-    this.socket.emit('online', { id: '000' });
 
     //  this.socket.on('update', (data: any) => {});
     this.socket.on('RECEIVE-MESSAGE', (data: any) => {
@@ -30,36 +31,41 @@ export class SocketService {
     this.socket.on('DELETED-MESSAGE', (data: any) => {
       console.log(data);
       this.chatService.currentChat = this.chatService.currentChat.filter(
-        (msg: any) => msg.Id != data
+        (msg: any) => msg.Id !== data.id
       );
     });
 
     this.socket.on(`NEW-MESSAGE`,async (data: any) => {
-      LocalNotifications.schedule({
-        notifications: [
-          {
-            id: 1,
-            title: 'Nuovo messaggio',
-            body: data,
-            schedule: { at: new Date(Date.now()) },
-            attachments: undefined,
-            actionTypeId: '',
-          }
-        ]
-      })
-      const newChannel: Channel = {
-        id: "1",
-        name: "pill-buddy-notifications",
-        description: "Channel for handling pill buddy notifications",
-        importance: 5,
-        visibility: 1,
-        vibration: true,
-        sound: "notif_bell.wav"
-      }
-      await LocalNotifications.createChannel(newChannel)
+      //if((this.permission.display === 'granted') || (await LocalNotifications.requestPermissions()).display === 'granted'){
+        LocalNotifications.schedule({
+          notifications: [
+            {
+              id: 1,
+              title: 'Nuovo messaggio',
+              body: data,
+              schedule: { at: new Date(Date.now()) },
+              actionTypeId: '',
+            }
+          ]
+        })
+        const newChannel: Channel = {
+          id: "1",
+          name: "orientamento-vallauri-chat",
+          description: "",
+          importance: 2,
+          visibility: 1,
+          vibration: true,
+          sound: "notif_bell.wav"
+        }
+        await LocalNotifications.createChannel(newChannel)
+      //}
       console.log(data);
       this.chatService.currentChat.unshift(data);
     });
+  }
+
+  async getPermission() {
+    this.permission = await LocalNotifications.requestPermissions()
   }
 
   sendMessage(message: any) {
