31cf147aadeecc6983eba97bd372161ce4b2fe4c
Pietro <p.cairo.2224@vallauri.edu>
2024-06-26 16:17:37 +0200

    Updated mobile version inserted in this branch

--- Mobile/src/app/service/data-storage.service.ts
+++ Mobile/src/app/service/data-storage.service.ts
@@ -1,50 +1,129 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, catchError, map } from 'rxjs';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class DataStorageService {
-  private REST_API_SERVER = 'http://80.180.105.71:80/api';
+  // 10.0.102.85
+  // localhost
+  private REST_API_SERVER = "http://localhost:3000/api";
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private router:Router, private alertController:AlertController) { }
 
-  public InviaRichiesta(
-    method: string,
-    resource: string,
-    params: any = {}
-  ): Observable<any> | undefined {
-    resource = this.REST_API_SERVER + resource;
+  public InviaRichiesta(method:string, resource:string, params:any = {}):Observable<any> | undefined {
+    resource = this.REST_API_SERVER + resource
 
-    switch (method.toLowerCase()) {
-      case 'get':
-        return this.httpClient.get(resource, { params: params });
+    switch(method.toLowerCase()) {
+      case "get":
+        return this.httpClient.get(resource, {params: params, observe: "response"}).pipe(map((response: HttpResponse<any>) => {
+          const authHeader = response.headers.get("authorization")
+          
+          localStorage.setItem("authToken", authHeader!)
 
-      case 'delete':
-        return this.httpClient.delete(resource, { body: params });
-
-      case 'post':
-        return this.httpClient.post(resource, params);
-
-      case 'put':
-        return this.httpClient.put(resource, params);
-
-      case 'login':
-        return this.httpClient
-          .post(resource, params, { observe: 'response' })
-          .pipe(
-            map((response: HttpResponse<any>) => {
-              const authHeader = response.headers.get('authorization');
-
-              localStorage.setItem('authToken', authHeader!);
-
-              return response.body;
+          return response.body
+        }), catchError(async (err) => {
+          if(err.status == 403) {
+            this.router.navigateByUrl("/login")
+  
+            const alert = await this.alertController.create({
+              header: "Errore",
+              message: "Sessione scaduta, effettua nuovamente il login",
+              buttons: [
+                {
+                  text:'OK',
+                }
+              ]
             })
-          );
+            await alert.present()
+          }
+
+          return new Observable<never>(); // Return an empty Observable
+        }))
+
+      case "delete":
+        return this.httpClient.delete(resource, {body: params, observe: "response"}).pipe(map((response: HttpResponse<any>) => {
+          const authHeader = response.headers.get("authorization")
+          
+          localStorage.setItem("authToken", authHeader!)
+
+          return response.body
+        }), catchError(async (err) => {
+          if(err.status == 403) {
+            this.router.navigateByUrl("/login")
+  
+            const alert = await this.alertController.create({
+              header: "Errore",
+              message: "Sessione scaduta, effettua nuovamente il login",
+              buttons: [
+                {
+                  text:'OK',
+                }
+              ]
+            })
+            await alert.present()
+          }
+
+          return new Observable<never>(); // Return an empty Observable
+        }))
+
+      case "post":
+        return this.httpClient.post(resource, params, { observe: "response" }).pipe(map((response: HttpResponse<any>) => {
+          const authHeader = response.headers.get("authorization")
+          
+          localStorage.setItem("authToken", authHeader!)
+
+          return response.body
+        }), catchError(async (err) => {
+          if(err.status == 403) {
+            this.router.navigateByUrl("/login")
+  
+            const alert = await this.alertController.create({
+              header: "Errore",
+              message: "Sessione scaduta, effettua nuovamente il login",
+              buttons: [
+                {
+                  text:'OK',
+                }
+              ]
+            })
+            await alert.present()
+          }
+
+          return new Observable<never>(); // Return an empty Observable
+        }))
+  
+      case "put":
+        return this.httpClient.put(resource, params, { observe: "response" }).pipe(map((response: HttpResponse<any>) => {
+          const authHeader = response.headers.get("authorization")
+          
+          localStorage.setItem("authToken", authHeader!)
+
+          return response.body
+        }), catchError(async (err) => {
+          if(err.status == 403) {
+            this.router.navigateByUrl("/login")
+  
+            const alert = await this.alertController.create({
+              header: "Errore",
+              message: "Sessione scaduta, effettua nuovamente il login",
+              buttons: [
+                {
+                  text:'OK',
+                }
+              ]
+            })
+            await alert.present()
+          }
+
+          return new Observable<never>(); // Return an empty Observable
+        }))
 
       default:
-        return undefined;
+        return undefined
     }
   }
 }
--- Mobile/src/app/service/socket.service.ts
+++ Mobile/src/app/service/socket.service.ts
@@ -1,65 +1,71 @@
 import { Injectable } from '@angular/core';
 import { io } from 'socket.io-client';
 import { ChatService } from './chat.service';
-import { Channel, LocalNotifications } from '@capacitor/local-notifications';
+import {Channel, LocalNotifications} from '@capacitor/local-notifications';
+import { Capacitor } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SocketService {
   socket: any;
+  permission: any
 
   constructor(protected chatService: ChatService) {}
 
   GoOnline() {
-    this.socket = io('http://80.180.105.71:80', {
+    this.socket = io('http://10.88.202.145:3001', {
       withCredentials: true,
       extraHeaders: {
         'my-custom-header': 'abcd',
       },
     });
 
+
     //  this.socket.on('update', (data: any) => {});
     this.socket.on('RECEIVE-MESSAGE', (data: any) => {
+      
       this.visualizzaMessaggio(data);
     });
 
     this.socket.on('DELETED-MESSAGE', (data: any) => {
       console.log(data);
-      console.log(this.chatService.currentChat);
       this.chatService.currentChat = this.chatService.currentChat.filter(
-        (message: any) => message.Id !== data.id
+        (msg: any) => msg.Id !== data.id
       );
-      console.log(this.chatService.currentChat);
     });
 
-    this.socket.on(`NEW-MESSAGE`, async (data: any) => {
-      //invia messaggio
-      LocalNotifications.schedule({
-        notifications: [
-          {
-            id: 1,
-            title: 'Nuovo messaggio',
-            body: data,
-            schedule: { at: new Date(Date.now()) },
-            attachments: undefined,
-            actionTypeId: '',
-          },
-        ],
-      });
-      const newChannel: Channel = {
-        id: '1',
-        name: 'pill-buddy-notifications',
-        description: 'Channel for handling pill buddy notifications',
-        importance: 5,
-        visibility: 1,
-        vibration: true,
-        sound: 'notif_bell.wav',
-      };
-      //await LocalNotifications.createChannel(newChannel);
+    this.socket.on(`NEW-MESSAGE`,async (data: any) => {
+      //if((this.permission.display === 'granted') || (await LocalNotifications.requestPermissions()).display === 'granted'){
+        LocalNotifications.schedule({
+          notifications: [
+            {
+              id: 1,
+              title: 'Nuovo messaggio',
+              body: data,
+              schedule: { at: new Date(Date.now()) },
+              actionTypeId: '',
+            }
+          ]
+        })
+        const newChannel: Channel = {
+          id: "1",
+          name: "orientamento-vallauri-chat",
+          description: "",
+          importance: 2,
+          visibility: 1,
+          vibration: true,
+          sound: "notif_bell.wav"
+        }
+        await LocalNotifications.createChannel(newChannel)
+      //}
       console.log(data);
       this.chatService.currentChat.unshift(data);
     });
+  }
+
+  async getPermission() {
+    this.permission = await LocalNotifications.requestPermissions()
   }
 
   sendMessage(message: any) {
