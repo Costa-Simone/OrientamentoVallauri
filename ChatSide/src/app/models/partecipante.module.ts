76b008b43fad378bc5478294529264a73cab8f5b
Costa Simone <s.costa.2235@vallauri.edu>
2024-06-27 21:45:48 +0200

    Update

--- ChatSide/src/app/models/partecipante.module.ts
+++
@@ -1,9 +0,0 @@
-export class Partecipante {
-    public IdGruppo:string
-    public IdStudente:number
-
-    constructor(idGruppo:string, idStudente:number) {
-        this.IdGruppo = idGruppo
-        this.IdStudente = idStudente
-    }
-}
